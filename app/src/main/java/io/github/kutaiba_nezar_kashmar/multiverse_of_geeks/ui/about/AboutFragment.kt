562c767aecf3f7fa2a79dcb7dd3a4d708166f739
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2022-03-27 11:52:54 +0200

    about layout added full

---
+++ app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/ui/about/AboutFragment.kt
@@ -0,0 +1,31 @@
+package io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.ui.about
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import io.github.kutaiba_nezar_kashmar.newapp.databinding.FragmentAboutBinding
+
+class AboutFragment : Fragment()
+{
+    private var _binding : FragmentAboutBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+    {
+        val aboutVM = ViewModelProvider(this).get(AboutViewModel::class.java)
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView()
+    {
+        super.onDestroyView()
+        _binding = null
+    }
+}
