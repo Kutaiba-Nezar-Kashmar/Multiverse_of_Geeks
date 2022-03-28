5cbff96ce7d66a533a11b00283b79a1a877a61d2
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2022-03-28 18:24:30 +0200

    edit profile layout added

---
+++ app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/ui/edit_profile/EditProfileViewModel.kt
@@ -0,0 +1,9 @@
+package io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.ui.edit_profile
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class EditProfileViewModel() : ViewModel()
+{
+
+}
---
+++ app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/util/CountryAdapter.kt
@@ -0,0 +1,6 @@
+package io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.util
+
+class CountryAdapter
+{
+
+}
