c9d49a177496be1b6518867a6d4baa1cfd4d4363
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2022-03-29 09:23:12 +0200

    navigation from MyProfileFragment to EditProfileFragment done

--- app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/ui/my_profile/MyProfileFragment.java
+++ app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/ui/my_profile/MyProfileFragment.java
@@ -4,20 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
-import io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.ui.edit_profile.EditProfileViewModel;
-import io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.ui.login.LoginViewModel;
-import io.github.kutaiba_nezar_kashmar.newapp.databinding.FragmentCreateProfileBinding;
-import io.github.kutaiba_nezar_kashmar.newapp.databinding.FragmentLoginBinding;
+import io.github.kutaiba_nezar_kashmar.newapp.R;
 import io.github.kutaiba_nezar_kashmar.newapp.databinding.FragmentMyProfileBinding;
 
-public class MyProfileFragment extends Fragment
+public class MyProfileFragment extends Fragment implements View.OnClickListener
 {
   private FragmentMyProfileBinding binding;
+  ImageView editIcon;
+
+  public MyProfileFragment()
+  {
+  }
 
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
@@ -27,7 +33,6 @@ public class MyProfileFragment extends Fragment
     binding = FragmentMyProfileBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
 
-
     return root;
   }
 
@@ -35,5 +40,19 @@ public class MyProfileFragment extends Fragment
   {
     super.onDestroyView();
     binding = null;
+  }
+
+  @Override public void onViewCreated(@NonNull View view,
+      @Nullable Bundle savedInstanceState)
+  {
+    super.onViewCreated(view, savedInstanceState);
+    Navigation.findNavController(view);
+    editIcon = (ImageView) view.findViewById(R.id.edit_icon_button);
+    editIcon.setOnClickListener(this);
+  }
+
+  @Override public void onClick(View view)
+  {
+    Navigation.findNavController(view).navigate(R.id.edit_profile_nav);
   }
 }
