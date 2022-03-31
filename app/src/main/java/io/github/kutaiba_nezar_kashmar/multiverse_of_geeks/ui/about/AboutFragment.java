acb3fc45337345b95095983456804dddec883a75
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2022-03-31 22:43:35 +0200

    Explicit intent added in the About fragment to navigate to relevant sites/apps on link click

--- app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/ui/about/AboutFragment.java
+++ app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/ui/about/AboutFragment.java
@@ -1,21 +1,27 @@
 package io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.ui.about;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.ui.edit_profile.EditProfileViewModel;
+import io.github.kutaiba_nezar_kashmar.newapp.R;
 import io.github.kutaiba_nezar_kashmar.newapp.databinding.FragmentAboutBinding;
-import io.github.kutaiba_nezar_kashmar.newapp.databinding.FragmentCreateProfileBinding;
 
 public class AboutFragment extends Fragment
 {
   private FragmentAboutBinding binding;
+  private TextView facebook;
+  private TextView github;
+  private TextView linkedin;
 
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState)
@@ -24,13 +30,41 @@ public class AboutFragment extends Fragment
         .get(EditProfileViewModel.class);
 
     binding = FragmentAboutBinding.inflate(inflater, container, false);
+    View root = binding.getRoot();
+    String action = Intent.ACTION_VIEW;
+    facebook = root.findViewById(R.id.facebook_link);
+    github = root.findViewById(R.id.github_link);
+    linkedin = root.findViewById(R.id.linkedin_link);
 
-    return binding.getRoot();
+    facebook.setOnClickListener(view ->
+    {
+      Intent intent = new Intent(action, navToLinkIntent("https://www.facebook.com/kutaiba.kashmar"));
+      startActivity(intent);
+    });
+
+    github.setOnClickListener(view ->
+    {
+      Intent intent = new Intent(action, navToLinkIntent("https://github.com/Kutaiba-Nezar-Kashmar"));
+      startActivity(intent);
+    });
+
+    linkedin.setOnClickListener(view ->
+    {
+      Intent intent = new Intent(action, navToLinkIntent("https://www.linkedin.com/in/kutaiba-nezar-kashmar-5380a81b5/"));
+      startActivity(intent);
+    });
+
+    return root;
   }
 
   @Override public void onDestroyView()
   {
     super.onDestroyView();
     binding = null;
+  }
+
+  private Uri navToLinkIntent(String uri)
+  {
+    return Uri.parse(uri);
   }
 }
