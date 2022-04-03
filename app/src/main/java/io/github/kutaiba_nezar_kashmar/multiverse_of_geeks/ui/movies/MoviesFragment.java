04c789d7ff099b340225d6f33c619a07bcc8db35
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2022-04-03 18:17:55 +0200

    clean up MoviesFragment.java

--- app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/ui/movies/MoviesFragment.java
+++ app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/ui/movies/MoviesFragment.java
@@ -4,11 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -16,14 +14,11 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
-
 import java.util.ArrayList;
 
 import io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.domain.Movie;
 import io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.util.movies.MoviesAdapter;
 import io.github.kutaiba_nezar_kashmar.newapp.R;
-import io.github.kutaiba_nezar_kashmar.newapp.databinding.FragmentLoginBinding;
 import io.github.kutaiba_nezar_kashmar.newapp.databinding.FragmentMoviesBinding;
 
 public class MoviesFragment extends Fragment
@@ -31,34 +26,15 @@ public class MoviesFragment extends Fragment
   private FragmentMoviesBinding binding;
   private RecyclerView recyclerView;
   private ArrayList<Movie> movies = new ArrayList<>();
+  private MoviesViewModel moviesViewModel;
 
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState)
   {
-    MoviesViewModel moviesViewModel = new ViewModelProvider(this)
-        .get(MoviesViewModel.class);
+    moviesViewModel = new ViewModelProvider(this).get(MoviesViewModel.class);
 
     binding = FragmentMoviesBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
-
-    moviesViewModel.getAllPopularMovies();
-
-    recyclerView = root.findViewById(R.id.movies_rv);
-    recyclerView.hasFixedSize();
-    recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
-
-    MoviesAdapter moviesAdapter = new MoviesAdapter(movies);
-    Observer<ArrayList<Movie>> update = moviesAdapter::updateMovieList;
-    moviesViewModel.getAllPopularMovies()
-        .observe(getViewLifecycleOwner(), update);
-
-
-
-    recyclerView.setAdapter(moviesAdapter);
-    moviesAdapter.setListener(movie -> {
-      NavHostFragment.findNavController(this)
-          .navigate(R.id.action_nav_movies_to_nav_single_movie);
-    });
 
     return root;
   }
@@ -67,5 +43,30 @@ public class MoviesFragment extends Fragment
   {
     super.onDestroyView();
     binding = null;
+  }
+
+  @Override public void onViewCreated(@NonNull View view,
+      @Nullable Bundle savedInstanceState)
+  {
+    moviesViewModel.getAllPopularMovies();
+
+    recyclerView = view.findViewById(R.id.movies_rv);
+    recyclerView.hasFixedSize();
+    recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+    setUpRecyclerView();
+  }
+
+  private void setUpRecyclerView()
+  {
+    MoviesAdapter moviesAdapter = new MoviesAdapter(movies);
+    Observer<ArrayList<Movie>> update = moviesAdapter::updateMovieList;
+    moviesViewModel.getAllPopularMovies()
+        .observe(getViewLifecycleOwner(), update);
+
+    recyclerView.setAdapter(moviesAdapter);
+    moviesAdapter.setListener(movie -> {
+      NavHostFragment.findNavController(this)
+          .navigate(R.id.action_nav_movies_to_nav_single_movie);
+    });
   }
 }
