fe5dfe4bea26383f6cb1f22cee18e434a557c0e4
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2022-04-03 17:05:01 +0200

    ratting bar added to movies

--- app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/util/movies/MoviesAdapter.java
+++ app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/util/movies/MoviesAdapter.java
@@ -1,15 +1,14 @@
 package io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.util.movies;
 
 import android.content.Context;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -20,7 +19,7 @@ import io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.domain.Movie;
 import io.github.kutaiba_nezar_kashmar.newapp.R;
 
 public class MoviesAdapter
-    extends RecyclerView.Adapter<MoviesAdapter.ViewHolder>
+    extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder>
 {
   private ArrayList<Movie> movies;
   private OnClickListener listener;
@@ -30,20 +29,21 @@ public class MoviesAdapter
     this.movies = movies;
   }
 
-  @NonNull @Override public ViewHolder onCreateViewHolder(
+  @NonNull @Override public MovieViewHolder onCreateViewHolder(
       @NonNull ViewGroup parent, int viewType)
   {
     LayoutInflater inflater = LayoutInflater.from(parent.getContext());
     View view = inflater.inflate(R.layout.movie_item, parent, false);
-    return new ViewHolder(view);
+    return new MovieViewHolder(view);
   }
 
-  @Override public void onBindViewHolder(@NonNull ViewHolder holder,
+  @Override public void onBindViewHolder(@NonNull MovieViewHolder holder,
       int position)
   {
     //Glide.with(this).load("https://image.tmdb.org/t/p/w500" + movie.getPoster_path()).into(imageView);
     holder.title.setText(movies.get(position).getTitle());
     Glide.with(holder.context).load("https://image.tmdb.org/t/p/w500" + movies.get(position).getPoster_path()).into(holder.poster);
+    holder.movieRating.setRating(movies.get(position).getVote_average());
   }
 
   public void updateMovieList(final ArrayList<Movie> movies)
@@ -67,18 +67,20 @@ public class MoviesAdapter
     this.listener = listener;
   }
 
-  class ViewHolder extends RecyclerView.ViewHolder
+  class MovieViewHolder extends RecyclerView.ViewHolder
   {
     private final TextView title;
     private final ImageView poster;
+    private final RatingBar movieRating;
     private Context context;
 
-    public ViewHolder(@NonNull View itemView)
+    public MovieViewHolder(@NonNull View itemView)
     {
       super(itemView);
       context = itemView.getContext();
       title = itemView.findViewById(R.id.movie_text_view);
       poster = itemView.findViewById(R.id.movie_image);
+      movieRating = itemView.findViewById(R.id.movie_rating_bar);
 
       itemView.setOnClickListener(view -> {
         listener.onClick(movies.get(getBindingAdapterPosition()));
