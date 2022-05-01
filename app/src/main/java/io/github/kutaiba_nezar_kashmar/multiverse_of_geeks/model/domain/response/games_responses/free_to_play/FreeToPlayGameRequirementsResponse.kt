9cd058b8aba1f44c3763ba71440c86b1fbbdbb56
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2022-05-01 22:28:39 +0200

    free to play games domain are in kotlin now

---
+++ app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/model/domain/response/games_responses/free_to_play/FreeToPlayGameRequirementsResponse.kt
@@ -0,0 +1,10 @@
+package io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.model.domain.response.games_responses.free_to_play
+
+class FreeToPlayGameRequirementsResponse
+{
+    val os : String = ""
+    val processor : String = ""
+    val memory : String = ""
+    val graphics : String = ""
+    val storage : String = ""
+}
---
+++ app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/model/domain/response/games_responses/free_to_play/FreeToPlayGameResponse.kt
@@ -0,0 +1,22 @@
+package io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.model.domain.response.games_responses.free_to_play
+
+import io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.model.domain.response.games_responses.games.GameScreenShots
+
+class FreeToPlayGameResponse
+{
+    val id : Int = 0
+    val title : String = ""
+    val thumbnail : String = ""
+    val status : String = ""
+    val short_description : String = ""
+    val description : String = ""
+    val game_url : String = ""
+    val genre : String = ""
+    val platform : String = ""
+    val publisher : String = ""
+    val developer : String = ""
+    val release_date : String = ""
+    val freetogame_profile_url : String = ""
+    val minimum_system_requirements : FreeToPlayGameRequirementsResponse = FreeToPlayGameRequirementsResponse()
+    val screenshots : ArrayList<GameScreenShots> = ArrayList()
+}
