6a7ce92320269fa375b8e8e3e6fbd384da37f449
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2022-05-02 18:20:51 +0200

    single tv/movie domains fix

--- app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/model/domain/response/media/movie_responses/SingleMovieResponse.kt
+++ app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/model/domain/response/media/movie_responses/SingleMovieResponse.kt
@@ -10,121 +10,49 @@ import io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.model.domain.response
 import java.util.ArrayList
 
 @Entity(tableName = "movie")
-class SingleMovieResponse
+data class SingleMovieResponse
+    (
+        var backdrop_path: String,
+        var budget: Int,
+        var homepage: String,
+        @PrimaryKey var id: Int,
+        var imdb_id: String,
+        var original_language: String,
+        var original_title: String,
+        var overview: String,
+        var popularity: Float,
+        var poster_path: String,
+        var revenue: Int,
+        var runtime: Int,
+        var status: String,
+        var tagline: String,
+        var title: String,
+        var video: String,
+        var vote_average: Float,
+        var vote_count: Int,
+)
 {
     @Ignore
-    val adult: Boolean = true
+    var adult: Boolean = false
 
     @Ignore
-    val belongs_to_collection: MovieCollectionResponse =
+    var belongs_to_collection: MovieCollectionResponse =
         MovieCollectionResponse()
-    var backdrop_path: String = ""
-        set(value)
-        {
-            field = value
-        }
-    var budget: Int = 0
-        set(value)
-        {
-            field = value
-        }
 
     @Ignore
     val genres: ArrayList<MediaGenreResponse> = ArrayList()
-    var homepage: String = ""
-        set(value)
-        {
-            field = value
-        }
-
-    @PrimaryKey
-    var id: Int = 0
-        set(value)
-        {
-            field = value
-        }
-    var imdb_id: String = ""
-        set(value)
-        {
-            field = value
-        }
-    var original_language: String = ""
-        set(value)
-        {
-            field = value
-        }
-    var original_title: String = ""
-        set(value)
-        {
-            field = value
-        }
-    var overview: String = ""
-        set(value)
-        {
-            field = value
-        }
-    var popularity: Float = 0.0F
-        set(value)
-        {
-            field = value
-        }
-    var poster_path: String = ""
-        set(value)
-        {
-            field = value
-        }
 
     @Ignore
-    val production_companies: ArrayList<MediaProductionCompaniesResponse> =
+    var production_companies: ArrayList<MediaProductionCompaniesResponse> =
         ArrayList()
 
     @Ignore
-    val release_date: String = ""
-    var revenue: Int = 0
-        set(value)
-        {
-            field = value
-        }
-    var runtime: Int = 0
-        set(value)
-        {
-            field = value
-        }
+    var release_date: String = ""
 
     @Ignore
-    val production_countries: ArrayList<MediaProductionCountriesResponse> =
+    var production_countries: ArrayList<MediaProductionCountriesResponse> =
         ArrayList()
 
     @Ignore
-    val spoken_languages: ArrayList<MediaSpokenLanguagesResponse> = ArrayList()
-    var status: String = ""
-        set(value)
-        {
-            field = value
-        }
-    var tagline: String = ""
-        set(value)
-        {
-            field = value
-        }
-    var title: String = ""
-        set(value)
-        {
-            field = value
-        }
-    var video: String = ""
-        set(value)
-        {
-            field = value
-        }
-    var vote_average: Float = 0.0F
-        set(value)
-        {
-            field = value
-        }
-    var vote_count: Int = 0
-        set(value)
-        {
-            field = value
-        }
+    var spoken_languages: ArrayList<MediaSpokenLanguagesResponse> = ArrayList()
 }
--- app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/model/domain/response/media/tv_responses/SingleTvShowResponse.kt
+++ app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/model/domain/response/media/tv_responses/SingleTvShowResponse.kt
@@ -10,126 +10,65 @@ import io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.model.domain.response
 import java.util.ArrayList
 
 @Entity(tableName = "tv")
-class SingleTvShowResponse
+data class SingleTvShowResponse
+    (
+        var backdrop_path: String,
+        var first_air_date: String,
+        var homepage: String,
+        @PrimaryKey var id: Int,
+        var in_production: Boolean,
+        var last_air_date: String,
+        var name: String,
+        var number_of_episodes: Int,
+        var number_of_seasons: Int,
+        var original_language: String,
+        var original_name: String,
+        var overview: String,
+        var popularity: Float,
+        var poster_path: String,
+        var status: String,
+        var tagline: String,
+        var type: String,
+        var vote_average: Float,
+        var vote_count: Float,
+)
 {
     @Ignore
-    val adult : Boolean = true
-    var backdrop_path : String = ""
-        set(value)
-        {
-            field = value
-        }
+    val adult: Boolean = false
+
     @Ignore
-    val created_by : ArrayList<TvShowCreatorResponse> = ArrayList()
+    val created_by: ArrayList<TvShowCreatorResponse> = ArrayList()
+
     @Ignore
-    val episode_run_time : ArrayList<Integer> = ArrayList()
-    var first_air_date : String = ""
-        set(value)
-        {
-            field = value
-        }
+    val episode_run_time: ArrayList<Integer> = ArrayList()
+
     @Ignore
-    val genres : ArrayList<MediaGenreResponse> = ArrayList()
-    var homepage : String = ""
-        set(value)
-        {
-            field = value
-        }
-    @PrimaryKey
-    var id : Int = 0
-        set(value)
-        {
-            field = value
-        }
-    var in_production : Boolean = true
-        set(value)
-        {
-            field = value
-        }
+    val genres: ArrayList<MediaGenreResponse> = ArrayList()
+
     @Ignore
-    val languages : ArrayList<String> = ArrayList()
-    var last_air_date : String = ""
-        set(value)
-        {
-            field = value
-        }
+    val languages: ArrayList<String> = ArrayList()
+
     @Ignore
-    val last_episode_to_air : TvShowLastEpisodeToAirResponse = TvShowLastEpisodeToAirResponse()
-    var name : String = ""
-        set(value)
-        {
-            field = value
-        }
+    val last_episode_to_air: TvShowLastEpisodeToAirResponse =
+        TvShowLastEpisodeToAirResponse()
+
     @Ignore
-    val networks : ArrayList<TvShowNetworkResponse> = ArrayList()
-    var number_of_episodes : Int = 0
-        set(value)
-        {
-            field = value
-        }
-    var number_of_seasons : Int = 0
-        set(value)
-        {
-            field = value
-        }
+    val networks: ArrayList<TvShowNetworkResponse> = ArrayList()
+
     @Ignore
-    val origin_country : ArrayList<String> = ArrayList()
-    var original_language : String = ""
-        set(value)
-        {
-            field = value
-        }
-    var original_name : String = ""
-        set(value)
-        {
-            field = value
-        }
-    var overview : String = ""
-        set(value)
-        {
-            field = value
-        }
-    var popularity : Float = 0.0F
-        set(value)
-        {
-            field = value
-        }
-    var poster_path : String = ""
-        set(value)
-        {
-            field = value
-        }
+    val origin_country: ArrayList<String> = ArrayList()
+
     @Ignore
-    val production_companies : ArrayList<MediaProductionCompaniesResponse> = ArrayList()
+    val production_companies: ArrayList<MediaProductionCompaniesResponse> =
+        ArrayList()
+
     @Ignore
-    val production_countries : ArrayList<MediaProductionCountriesResponse> = ArrayList()
+    val production_countries: ArrayList<MediaProductionCountriesResponse> =
+        ArrayList()
+
     @Ignore
-    val seasons : ArrayList<TvShowSeasonResponse> = ArrayList()
+    val seasons: ArrayList<TvShowSeasonResponse> = ArrayList()
+
     @Ignore
-    val spoken_languages : ArrayList<MediaSpokenLanguagesResponse> = ArrayList()
-    var status : String = ""
-        set(value)
-        {
-            field = value
-        }
-    var tagline : String = ""
-        set(value)
-        {
-            field = value
-        }
-    var type : String = ""
-        set(value)
-        {
-            field = value
-        }
-    var vote_average : Float = 0.0F
-        set(value)
-        {
-            field = value
-        }
-    var vote_count: Float = 0.0F
-        set(value)
-        {
-            field = value
-        }
+    val spoken_languages: ArrayList<MediaSpokenLanguagesResponse> = ArrayList()
 }
