22b6607fb3bc72073d7667fbb413b662913a34a3
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2022-05-04 21:34:56 +0200

    .

--- app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/ui/login/LoginFragment.java
+++ app/src/main/java/io/github/kutaiba_nezar_kashmar/multiverse_of_geeks/ui/login/LoginFragment.java
@@ -1,5 +1,6 @@
 package io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.ui.login;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,29 +18,26 @@ import androidx.navigation.Navigation;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import io.github.kutaiba_nezar_kashmar.multiverse_of_geeks.MainActivity;
 import io.github.kutaiba_nezar_kashmar.newapp.R;
 import io.github.kutaiba_nezar_kashmar.newapp.databinding.FragmentLoginBinding;
 
 public class LoginFragment extends Fragment
 {
   private FragmentLoginBinding binding;
-  private LoginViewModel loginViewModel;
-  private EditText emailField;
-  private EditText passwordField;
-  private Button loginButton;
-  private TextView toCreateAccount;
+  private SignUnViewModel signUnViewModel;
+  private Button signOut;
 
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState)
   {
-    loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+    signUnViewModel = new ViewModelProvider(this).get(SignUnViewModel.class);
 
     binding = FragmentLoginBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
-    emailField = root.findViewById(R.id.login_email_field);
-    passwordField = root.findViewById(R.id.login_password_field);
-    loginButton = root.findViewById(R.id.login_button);
-    toCreateAccount = root.findViewById(R.id.navigate_to_create_account_link);
+    signOut = root.findViewById(R.id.logout);
+    checkIfSignedIn();
+    signOut.setOnClickListener(this::signOut);
     return root;
   }
 
@@ -50,47 +48,25 @@ public class LoginFragment extends Fragment
     binding = null;
   }
 
-  @Override
-  public void onViewCreated(@NonNull View view,
-      @Nullable Bundle savedInstanceState)
+  private void checkIfSignedIn()
   {
-    checkIfSignedIn(view);
-    login(view);
-
-    toCreateAccount.setOnClickListener(view1 -> {
-      Navigation.findNavController(view)
-          .navigate(LoginFragmentDirections.actionNavLoginToNavCreateAccount());
-    });
-  }
-
-  private void login(View view)
-  {
-    String email = this.emailField.getText().toString();
-    String password = this.passwordField.getText().toString();
-    if (!email.isEmpty() && !password.isEmpty())
-    {
-      loginViewModel.login(email, password);
-      loginButton.setOnClickListener(view1 -> {
-        Navigation.findNavController(view)
-            .navigate(LoginFragmentDirections.actionNavLoginToNavHome());
-      });
-    }
-    else
-    {
-      Toast.makeText(getContext(), "email and password are required",
-          Toast.LENGTH_SHORT).show();
-    }
-  }
-
-  private void checkIfSignedIn(View view)
-  {
-    loginViewModel.getCurrentUser()
+    signUnViewModel.getCurrentUser()
         .observe(getViewLifecycleOwner(), firebaseUser -> {
           if (firebaseUser != null)
           {
-            Navigation.findNavController(view)
-                .navigate(LoginFragmentDirections.actionNavLoginToLogoutNav());
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            startActivity(intent);
+          }
+          else
+          {
+            Intent intent = new Intent(getActivity(), SignInActivity.class);
+            startActivity(intent);
           }
         });
+  }
+
+  private void signOut(View view)
+  {
+    signUnViewModel.signOut();
   }
 }
